c5712cb52a503d1bdd96ab57ce66c14ab6039944
Tipper Langseth <lang0981@umn.edu>
2015-08-17 18:33:51 -0500

    Mason

---
+++ RiotConsts.py
@@ -0,0 +1,46 @@
+URL = {
+    # Proxy may need to say global
+    'base': 'https://{proxy}.api.pvp.net/api/lol/{region}/{url}',
+    # "matchlist could be wrong
+    'match_by_matchID': 'v{version}/match/{matchId}'
+}
+
+API_VERSIONS = {
+    'match': '2.2'
+}
+
+REGIONS = {
+    'Brazil': 'br',
+    'EU Nordic & East': 'eune',
+    'EU West': 'euw',
+    'Korea': 'kr',
+    'Latin America North': 'lan',
+    'Latin America South': 'las',
+    'North America': 'na',
+    'Oceania': 'oce',
+    'Russia': 'ru',
+    'Turkey': 'tr'
+}
+
+api_key = '6e9e8cb9-8f9a-46ca-a698-028c3bc5ea85'
+
+ITEMS = {
+    'Amplifying Tome': 1052,
+    'Archangels Staff': 3003,
+    'Athenes Unholy Grail': 3174,
+    'Blasting Wand': 1026,
+    'Liandrys Torment': 3151,
+    'Ludens Echo': 3285,
+    'Magus': 3708,
+    'Morellonomicon': 3165,
+    'Nashors Tooth': 3115,
+    'Needlessly Large Rod': 1058,
+    'Rabadons Deathcap': 3089,
+    'Rod of Ages': 3027,
+    'Runeglaive': 3716,
+    'Rylais Crystal Scepter': 3116,
+    'Seraphs Embrace': 3048,
+    'Void Staff': 3135,
+    'Will of the Ancients': 3152,
+    'Zhonyas Hourglass': 3157
+}
