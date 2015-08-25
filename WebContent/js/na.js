fe07d39ff6964d1026f0d5a0b579c54e5b9fbc4d
Tipper Langseth <lang0981@umn.edu>
2015-08-25 18:18:32 -0500

    new way to test json

--- WebContent/js/na.js
+++ WebContent/js/na.js
@@ -1,7 +1,21 @@
-var jsonData = $.getJSON("../../item_analysis.json", function(jsonData){
-    console.log("success");
-    console.log(jsonData.patches.queues.regions[0].items[0].stats[0]);
-});
+request = new XMLHttpRequest();
+request.open('GET', '../../item_analysis.json', true);
+
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400){
+    // Success!
+    data = JSON.parse(request.responseText);
+  } else {
+    // We reached our target server, but it returned an error
+
+  }
+};
+
+request.onerror = function() {
+  // There was a connection error of some sort
+};
+
+request.send();
 
 
 
