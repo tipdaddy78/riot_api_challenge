11685ebcb87cad04765af7d49938e70808ca537b
Tipper Langseth <lang0981@umn.edu>
2015-08-25 20:27:07 -0500

    didn't work :(

--- WebContent/js/na.js
+++ WebContent/js/na.js
@@ -21,7 +21,7 @@ request.onload = function() {
     // Success!
     data = JSON.parse(request.responseText);
     for(i = 0; i < items.length; i++) {
-        rankedPickData511[i] = ((data.patches[1].queues[0].regions['North America'].items[i].stats[4].pick_rate)*100).toFixed(2);
+        rankedPickData511[i] = ((data.patches[1].queues[0].regions[1].items[i].stats[4].pick_rate)*100).toFixed(2);
         rankedPickData514[i] = ((data.patches[0].queues[0].regions[1].items[i].stats[4].pick_rate)*100).toFixed(2);
         rankedWinData511[i] = ((data.patches[1].queues[0].regions[1].items[i].stats[4].win_rate)*100).toFixed(2);
         rankedWinData514[i] = ((data.patches[0].queues[0].regions[1].items[i].stats[4].win_rate)*100).toFixed(2);
