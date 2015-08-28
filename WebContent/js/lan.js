92675421f5ef31fecdeabbfc650125b5073bc859
Tipper Langseth <lang0981@umn.edu>
2015-08-28 16:58:23 -0500

    Hopefully....

--- WebContent/js/lan.js
+++ WebContent/js/lan.js
@@ -214,7 +214,7 @@ window.onload = function(){
                 index += 5;
             }
         } else if (selLane != "" && selChampion != "") { // Both Lane and Champion Specified
-            if (selItem == ""){ // On initial load it will go into this block so stop it from trying to generate a graph.
+            if (selItem == null){ // On initial load it will go into this block so stop it from trying to generate a graph.
                 
             } else {
                 var index = 5;
