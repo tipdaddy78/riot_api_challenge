489da65beb939035a58c33fe025a77f3ab4ef3c3
Tipper Langseth <lang0981@umn.edu>
2015-08-27 20:32:06 -0500

    Argument retrieval

--- WebContent/js/lan.js
+++ WebContent/js/lan.js
@@ -15,8 +15,29 @@ window.onload = function(){
     var options = {scaleFontColor: "#fff", scaleLineColor : "#fff", scaleShowHorizontalLines: false,
         scaleShowVerticalLines: false, responsive: true, multiTooltipTemplate: "<%= value %>%", scaleBeginAtZero: false};
 
-    var champion = get("chmpion")
-    alert(champion);
+    var QueryString = function () {
+      // This function is anonymous, is executed immediately and 
+      // the return value is assigned to QueryString!
+      var query_string = {};
+      var query = window.location.search.substring(1);
+      var vars = query.split("&");
+      for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+            // If first entry with this name
+        if (typeof query_string[pair[0]] === "undefined") {
+          query_string[pair[0]] = decodeURIComponent(pair[1]);
+            // If second entry with this name
+        } else if (typeof query_string[pair[0]] === "string") {
+          var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+          query_string[pair[0]] = arr;
+            // If third or later entry with this name
+        } else {
+          query_string[pair[0]].push(decodeURIComponent(pair[1]));
+        }
+      } 
+        return query_string;
+    }();
+    alert(QueryString.champion);
 
     /*Load JSON file and fill arrays with data*/
     request = new XMLHttpRequest();
